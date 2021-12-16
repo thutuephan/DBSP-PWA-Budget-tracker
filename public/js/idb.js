dea1a7888d9a1bbeebc3cabb378c3eef29a01ce3
unknown <alicebe.phan@gmail.com>
2021-12-16 01:43:45 -0600

    finished up the code for saveRecord/offline and loadTransacation/online functionalities

--- public/js/idb.js
+++ public/js/idb.js
@@ -1,8 +1,9 @@
-let db;
-let budgetTracker;
+const { response } = require("express");
 
-// Create a new db request for a "budget" database.
-const request = indexedDB.open('BudgetDB', budgetTracker || 21);
+let db;
+
+// Create a new db request for a 'budget' database.
+const request = indexedDB.open('BudgetDB', 1);
 
 request.onupgradeneeded = function (e) {
   console.log('Upgrade needed in IndexDB');
@@ -15,78 +16,84 @@ request.onupgradeneeded = function (e) {
   db = e.target.result;
 
   if (db.objectStoreNames.length === 0) {
-    db.createObjectStore('newBudget', { autoIncrement: true });
+    db.createObjectStore('BudgetStore', { autoIncrement: true });
   }
 };
 
-// request.onerror = function (e) {
-//   console.log(`Woops! ${e.target.errorCode}`);
-// };
-
-// function checkDatabase() {
-//   console.log('check db invoked');
-
-//   // Open a transaction on your BudgetStore db
-//   let transaction = db.transaction(['BudgetStore'], 'readwrite');
-
-//   // access your BudgetStore object
-//   const store = transaction.objectStore('BudgetStore');
-
-//   // Get all records from store and set to a variable
-//   const getAll = store.getAll();
-
-//   // If the request was successful
-//   getAll.onsuccess = function () {
-//     // If there are items in the store, we need to bulk add them when we are back online
-//     if (getAll.result.length > 0) {
-//       fetch('/api/transaction/bulk', {
-//         method: 'POST',
-//         body: JSON.stringify(getAll.result),
-//         headers: {
-//           Accept: 'application/json, text/plain, */*',
-//           'Content-Type': 'application/json',
-//         },
-//       })
-//         .then((response) => response.json())
-//         .then((res) => {
-//           // If our returned response is not empty
-//           if (res.length !== 0) {
-//             // Open another transaction to BudgetStore with the ability to read and write
-//             transaction = db.transaction(['BudgetStore'], 'readwrite');
-
-//             // Assign the current store to a variable
-//             const currentStore = transaction.objectStore('BudgetStore');
-
-//             // Clear existing entries because our bulk add was successful
-//             currentStore.clear();
-//             console.log('Clearing store 🧹');
-//           }
-//         });
-//     }
-//   };
-// }
-
 request.onsuccess = function(e) {
-    db = e.target.result;
-    
-    // check if app is online before reading from db
-    if (navigator.onLine) {
-        console.log('Backend online!');
-        loadTransaction();
-    }
+  db = e.target.result;
+  
+  // check if app is online before reading from db
+  if (navigator.onLine) {
+      console.log('Backend online!');
+      checkDatabase();
+  }
 };
 
-// const saveRecord = (record) => {
-//   console.log('Save record invoked');
-//   // Create a transaction on the BudgetStore db with readwrite access
-//   const transaction = db.transaction(['BudgetStore'], 'readwrite');
 
-//   // Access your BudgetStore object store
-//   const store = transaction.objectStore('BudgetStore');
+request.onerror = function (e) {
+  // log error here
+  console.log(`Something went wrong! ${e.target.errorCode}`);
+};
 
-//   // Add record to your store with add method.
-//   store.add(record);
-// };
+function saveRecord(record) {
+  // this is actually where we save the data while we're offline
+  const transaction = db.transaction(['BudgetStore'], 'readwrite');
+  const budgetObjectStore = transaction.objectStore('BudgetStore');
+
+  // Add record to your store with add method
+  budgetObjectStore.add(record);
+
+}
+
+function checkDatabase() {
+  console.log('check db invoked');
+
+  // Open a transaction on BudgetStore db
+  let transaction = db.transaction(['BudgetStore'], 'readwrite');
+
+  // access BudgetStore object
+  const budgetObjectStore = transaction.objectStore('BudgetStore');
+
+  // get all records from store and set to a variable
+  const getAll = budgetObjectStore.getAll();
+  
+  getAll.onsuccess = function () {
+    
+    // check to see if we have result/data inside of indexedDB, send data to the api server; in other words, if somebody enters  while we're offline, that should have been saved to indexedDB and then when we come online, this function checkDatabase should be called and it's gonna check to see if there's anything they enter while offline. Then it will make a fetch request to this endpoint api/transaction/bulk
+    if(getAll.result.length > 0) {
+      fetch('api/transaction/bulk', {
+        method: 'POST',
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: 'application/json, text/plain, */*',
+          'Content-Type': 'application/json',
+        },
+      })
+        .then((response) => response.json())
+        .then((result) => {
+          // if successful, open a transaction on pending db
+          // access pending object store
+          // clear all items in store
+
+          // if our return is not emppty
+          if (result.length !== 0) {
+            // open another transaction to BudgetStore with the ability to read and write
+            transaction = db.transaction(['BudgetStore'], 'readwrite');
+            // assign the current store to a variable
+            const currentBudgetObjectStore = transaction.objectStore('BudgetStore');
+
+            // clear existing entries because our bulk add was successful
+            currentBudgetObjectStore.clear();
+            console.log('Clearing store!');
+          }
+
+        })
+
+    }
+  }
+}
+
 
 // Listen for app coming back online
-window.addEventListener('online', loadTransaction);
+window.addEventListener('online', checkDatabase);
